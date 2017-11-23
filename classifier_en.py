b9327c6a4e035c8916b0f13ccda6086a3ec52e12
Azzahid <azzahidadhityas@gmail.com>
2017-11-23 12:35:08 +0700

    classifier done

--- classifier_en.py
+++ classifier_en.py
@@ -2,82 +2,97 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from nltk.corpus import sentiwordnet as swn
 from nltk import sent_tokenize, word_tokenize, pos_tag
+from nltk.sentiment.util import mark_negation
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.base import TransformerMixin
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+import re
+from sklearn.model_selection import train_test_split
 
-def penn_to_wn(tag):
-    """
-    Convert between the PennTreebank tags to simple Wordnet tags
-    """
-    if tag.startswith('J'):
-        return wn.ADJ
-    elif tag.startswith('N'):
-        return wn.NOUN
-    elif tag.startswith('R'):
-        return wn.ADV
-    elif tag.startswith('V'):
-        return wn.VERB
-    return None
+def train(clf, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=33)
+    # print(X_train)
+    clf.fit(X_train, y_train)
+    print ("Accuracy: %s" % clf.score(X_test, y_test))
+    print(clf.named_steps['vectorizer'].get_feature_names())
+    name = clf.get_params()
+    joblib.dump(clf, 'clf-'+name['memory']+'.pkl')
 
+def stemming_tokenizer(sentence):
+    stemmer = PorterStemmer()
+    sentence = word_tokenize(sentence)
 
-def clean_text(text):
-    text = text.replace("<br />", " ")
-    text = text.decode("utf-8")
-
-    return text
-
-
-def swn_polarity(text):
-    """
-    Return a sentiment polarity: 0 = negative, 1 = positive
-    """
-    sentiment = 0.0
-
-    raw_sentences = sent_tokenize(text)
-    for raw_sentence in raw_sentences:
-        tagged_sentence = pos_tag(word_tokenize(raw_sentence))
-
-        for word, tag in tagged_sentence:
-            wn_tag = penn_to_wn(tag)
-            if wn_tag not in (wn.NOUN, wn.ADJ, wn.ADV):
-                continue
-
-            lemma = lemmatizer.lemmatize(word, pos=wn_tag)
-            if not lemma:
-                continue
-
-            synsets = wn.synsets(lemma, pos=wn_tag)
-            if not synsets:
-                continue
-
-            # Take the first sense, the most common
-            synset = synsets[0]
-            swn_synset = swn.senti_synset(synset.name())
-
-            sentiment += swn_synset.pos_score() - swn_synset.neg_score()
-            tokens_count += 1
-
-    # judgment call ? Default to positive or negative
-    if not tokens_count:
-        return 0
-
-    # sum greater than 0 => positive sentiment
-    if sentiment >= 0:
-        return 1
-
-    # negative sentiment
-    return 0
+    # newsentence = []
+    # for word in sentence :
+    #     if len(word) not in [0,1,2] :
+    #         newsentence.append(word)
+    #
+    # newsentence = mark_negation(newsentence)
+    # print(newsentence)
+    return mark_negation([stemmer.stem(w) for w in word_tokenize(sentence)])
 
 
-# Since we're shuffling, you'll get diffrent results
-print swn_polarity(train_X[0]), train_y[0] # 1 1
-print swn_polarity(train_X[1]), train_y[1] # 0 0
-print swn_polarity(train_X[2]), train_y[2] # 0 1
-print swn_polarity(train_X[3]), train_y[3] # 1 1
-print swn_polarity(train_X[4]), train_y[4] # 1 1`
+
+def createClassifier() :
+    file = open("resource/rt-polarity-pos", encoding = "ISO-8859-1")
+    sentences = []
+    expected = []
+
+    # for line in file:
+    #     print(line)
+
+    for line in file :
+        # line = line.decode('utf-8')
+        line = line.strip()
+        line = re.sub(r'[^\w\s]|[0-9]', ' ', line)
+        line = re.sub(' +',' ', line)
+        sentences.append(line)
+        expected.append(1)
+
+
+    file.close()
+
+    file = open("resource/rt-polarity-neg", encoding = "ISO-8859-1")
+
+    for line in file :
+        line = line.strip()
+        line = re.sub(r'[^\w\s]|[0-9]', ' ', line)
+        line = re.sub(' +',' ', line)
+        sentences.append(line)
+        expected.append(0)
+
+    file.close()
+
+    classifier = LogisticRegression()
+
+    clf = Pipeline(
+        [
+            ('vectorizer', CountVectorizer(analyzer="word",
+                                           ngram_range=(1,2),
+                                           tokenizer=stemming_tokenizer,
+                                           stop_words=stopwords.words('english'),
+                                           encoding="ISO-8859-1"
+                                           )),
+            ('classifier', classifier)
+        ],
+        type(classifier).__name__
+    )
+
+    train(clf, sentences, expected)
+#
+# def predictSentence() :
+#
 
 
 def main() :
-    file = open("rt-polarity.pos", "w+")
-    lemmatizer = WordNetLemmatizer()
+    createClassifier()
+
+
 
 
 
