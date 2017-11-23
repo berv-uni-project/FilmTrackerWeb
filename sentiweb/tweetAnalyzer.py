995a90bdec540e9c268f932d2a636827c10ae8d5
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-11-23 21:15:53 +0700

    indent

--- sentiweb/tweetAnalyzer.py
+++ sentiweb/tweetAnalyzer.py
@@ -22,79 +22,79 @@ class TweetAnalyzer:
     threshold = 0.6
 
     def predictTweets(tweets, clf):
-    return clf.predict(tweets)
+        return clf.predict(tweets)
 
     def stemming_tokenizer(sentence):
-    stemmer = PorterStemmer()
-    sentence = word_tokenize(sentence)
-    stop_words = set(stopwords.words('english'))
-    newsentence = []
-    for word in sentence:
-        if word not in stop_words:
-            if len(word) >= 3:
-                newsentence.append(word)
+        stemmer = PorterStemmer()
+        sentence = word_tokenize(sentence)
+        stop_words = set(stopwords.words('english'))
+        newsentence = []
+        for word in sentence:
+            if word not in stop_words:
+                if len(word) >= 3:
+                    newsentence.append(word)
 
-    newsentence = mark_negation(newsentence)
-    return newsentence
+        newsentence = mark_negation(newsentence)
+        return newsentence
 
     def predictProbaTweets(tweets, clf):
-    if "predict_proba" in dir(clf):
-        return clf.predict_proba(tweets)
-    else:
-        raise Exception('Error use Logistic Regression classifier')
+        if "predict_proba" in dir(clf):
+            return clf.predict_proba(tweets)
+        else:
+            raise Exception('Error use Logistic Regression classifier')
 
     def labelTweets(tweets, proba):
-    newtweetsPos = []
-    countPos = 0
-    newtweetsNeg = []
-    countNeg = 0
-    newtweetsUnknown = []
-    countUnknown = 0
-    for i in range(0, len(tweets)):
-        newtweets = tweets[i]
-        label = 0
-        if proba[i][0] >= threshold:
-            label = 1
-        else:
-            if proba[i][1] >= threshold:
-                label = 2
-        if label == 0:
-            newtweetsUnknown.append(newtweets)
-            countUnknown += 1
-        elif label == 1:
-            newtweetsPos.append(newtweets)
-            countPos += 1
-        else:
-            newtweetsNeg.append(newtweets)
-            countNeg += 1
+        newtweetsPos = []
+        countPos = 0
+        newtweetsNeg = []
+        countNeg = 0
+        newtweetsUnknown = []
+        countUnknown = 0
+        for i in range(0, len(tweets)):
+            newtweets = tweets[i]
+            label = 0
+            if proba[i][0] >= threshold:
+                label = 1
+            else:
+                if proba[i][1] >= threshold:
+                    label = 2
+            if label == 0:
+                newtweetsUnknown.append(newtweets)
+                countUnknown += 1
+            elif label == 1:
+                newtweetsPos.append(newtweets)
+                countPos += 1
+            else:
+                newtweetsNeg.append(newtweets)
+                countNeg += 1
 
-    return {
-        'text': {
-            'pos': newtweetsPos, 'neg': newtweetsNeg, 'unknown': newtweetsUnknown
-        },
-        'count': {
-            'pos': countPos, 'neg': countNeg, 'unknown': countUnknown
+        return {
+            'text': {
+                'pos': newtweetsPos, 'neg': newtweetsNeg, 'unknown': newtweetsUnknown
+            },
+            'count': {
+                'pos': countPos, 'neg': countNeg, 'unknown': countUnknown
+            }
         }
-    }
 
     def analyzeTweet(id, query):
-    clf = joblib.load('model/clf-LogisticRegression-100.pkl')
+        clf = joblib.load('model/clf-LogisticRegression-100.pkl')
 
-    tweets_ori = dataProcessor.requestDataFromAPI('en', query, 100)
-    tweets = tweets_ori['tweet']
-    for i in range(0, len(tweets)):
-        tweets[i] = dataProcessor.cleanTotalTweet(tweets[i], query)
+        tweets_ori = dataProcessor.requestDataFromAPI('en', query, 100)
+        tweets = tweets_ori['tweet']
+        for i in range(0, len(tweets)):
+            tweets[i] = dataProcessor.cleanTotalTweet(tweets[i], query)
 
-    proba = [""] * len(tweets)
-    proba = predictProbaTweets(tweets, clf)
+        proba = [""] * len(tweets)
+        proba = predictProbaTweets(tweets, clf)
 
-    dataTweets = labelTweets(tweets_ori['tweet'], proba)
-    message = {}
-    message['id'] = id
-    message['query'] = query
-    message['hasil'] = dataTweets
+        dataTweets = labelTweets(tweets_ori['tweet'], proba)
+        message = {}
+        message['id'] = id
+        message['query'] = query
+        message['hasil'] = dataTweets
 
-    return message
+        return message
 
 
 if __name__ == '__main__':
