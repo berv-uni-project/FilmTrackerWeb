7fc73133fb4b2240832173bc286c8afa91cae161
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-11-23 16:29:11 +0700

    change to popular

--- sentiweb/views.py
+++ sentiweb/views.py
@@ -5,9 +5,8 @@ import json
 
 
 def index(request):
-    url_film = 'http://twitter-crawler-data.herokuapp.com/api/release'
+    url_film = 'http://twitter-crawler-data.herokuapp.com/api/popular'
     image_base = 'https://image.tmdb.org/t/p/w500/'
-    payload = {'mode': 0, 'query': ''}
-    req = requests.get(url_film, params=payload)
+    req = requests.get(url_film)
     jsonData = json.loads(req.text)
     return render(request, 'index.html', {'film_data': jsonData['movies'], 'image_base': image_base})
