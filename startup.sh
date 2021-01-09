c1502337185d8358dfbe9375476110120acc46ab
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-09 17:14:46 +0700

    update log level

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn "filmesenti.wsgi" -b "0.0.0.0:${PORT}" --log-file -
+gunicorn "filmesenti.wsgi" -b "0.0.0.0:${PORT}" --log-file - --log-level debug
