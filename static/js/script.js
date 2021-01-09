f14fd535dd2491525dfb3832b25351345dbb5ef7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-09 09:19:52 +0700

    Change to autoinit

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,1 @@
-(function($){
-  $(function(){
-	    $('.sidenav').sidenav();
-      $('.collapsible').collapsible();
-      $('.materialboxed').materialbox();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+M.AutoInit()
