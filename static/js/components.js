a4c86643d97b5c63fbe8c0fe33462e355745d3e5
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-09 10:03:20 +0700

    Add components and bind with new vue

---
+++ static/js/components.js
@@ -0,0 +1,27 @@
+Vue.component('FilmList', {
+  props: ['filmData', 'imageBase'],
+  template: `
+  <ul class="collapsible">
+    <li v-for="movie in filmData" v-bind:key="movie.id">
+      <div class="collapsible-header">{{movie.title}}</div>
+      <div class="collapsible-body">
+        <div class="row">
+          <div class="col s12 m4 l2">
+            <img class="materialboxed responsive-img" src="{{imageBase}}{{movie.poster_path}}">
+          </div>
+          <div class="col s12 m8 l4">
+            <h4>{{ movie.title }}</h4>
+            <h5>Overview</h5>
+            <p>{{ movie.overview }}</p>
+            <h5>Release Date</h5>
+            <p>{{ movie.release_date}}</p>
+          </div>
+          <div class="col s12 m12 l6">
+              <canvas id="trend-{{ movie.id }}"></canvas>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+  `
+});
